b0cb8f5ff3c361e46fc3c151f428e7bcfdf51e8a
khalil <86266398+2Khalil@users.noreply.github.com>
2022-11-08 16:56:44 -0600

    Update LinoriaLib.lua

--- LinoriaLib.lua
+++ LinoriaLib.lua
@@ -10,7 +10,16 @@ local Mouse = LocalPlayer:GetMouse();
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
+-- made by synocrypts
+
+local letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"}
+local numbers = {1,2,3,4,5,6,7,8,9,0,10,11,12,12,13,14,15,16,17,18,19}
+local symbols = {"!","@","#","$","%","^","&","*","(",")","'","/","{","}"}
+
 local ScreenGui = Instance.new('ScreenGui');
+
+ScreenGui.Name = (letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)])
+
 ProtectGui(ScreenGui);
 
 ScreenGui.ZIndexBehavior = Enum.ZIndexBehavior.Global;
