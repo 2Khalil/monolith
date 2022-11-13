2fb22b4ac25d5a217576228d7674023f7502ce3f
khalil <86266398+2Khalil@users.noreply.github.com>
2022-11-12 23:53:35 -0600

    Update LinoriaLib.lua

--- LinoriaLib.lua
+++ LinoriaLib.lua
@@ -10,8 +10,6 @@ local Mouse = LocalPlayer:GetMouse();
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
--- made by synocrypts
-
 local letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"}
 local numbers = {1,2,3,4,5,6,7,8,9,0,10,11,12,12,13,14,15,16,17,18,19}
 local symbols = {"!","@","#","$","%","^","&","*","(",")","'","/","{","}"}
