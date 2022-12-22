c15f7d25fa3e4941680719444a2647c9612504dd
khalil <86266398+2Khalil@users.noreply.github.com>
2022-12-21 21:49:43 -0600

    Update LinoriaLib.lua

--- LinoriaLib.lua
+++ LinoriaLib.lua
@@ -2,13 +2,13 @@ local letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p"
 local numbers = {1,2,3,4,5,6,7,8,9,0,10,11,12,12,13,14,15,16,17,18,19}
 local symbols = {"!","@","#","$","%","^","&","*","(",")","'","/","{","}"}
 
-local ScreenGui = Instance.new('ScreenGui');
-
 local gen = (letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)])
 
 if CoreGui:FindFirstChild("solitution."..gen) then
     CoreGui["solitution."..gen]:Destroy()
 end
+
+local ScreenGui = Instance.new('ScreenGui');
 
 local InputService = game:GetService('UserInputService');
 local TextService = game:GetService('TextService');
