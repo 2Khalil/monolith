085b950ddd432c9c75b9425d9c092b28122db18a
khalil <86266398+2Khalil@users.noreply.github.com>
2022-12-21 21:51:48 -0600

    Update LinoriaLib.lua

--- LinoriaLib.lua
+++ LinoriaLib.lua
@@ -1,11 +1,5 @@
-local letters = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","y","z"}
-local numbers = {1,2,3,4,5,6,7,8,9,0,10,11,12,12,13,14,15,16,17,18,19}
-local symbols = {"!","@","#","$","%","^","&","*","(",")","'","/","{","}"}
-
-local gen = (letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..numbers[math.random(1,#numbers)]..numbers[math.random(1,#numbers)]..letters[math.random(1,#letters)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)]..symbols[math.random(1,#symbols)]..letters[math.random(1,#letters)]..symbols[math.random(1,#symbols)])
-
-if CoreGui:FindFirstChild("solitution."..gen) then
-    CoreGui["solitution."..gen]:Destroy()
+if CoreGui:FindFirstChild("solitution") then
+    CoreGui.solitution:Destroy()
 end
 
 local ScreenGui = Instance.new('ScreenGui');
@@ -22,7 +16,7 @@ local Mouse = LocalPlayer:GetMouse();
 
 local ProtectGui = protectgui or (syn and syn.protect_gui) or (function() end);
 
-local guiname = "solitution."..gen
+local guiname = "solitution"
 
 ScreenGui.Name = guiname
 
